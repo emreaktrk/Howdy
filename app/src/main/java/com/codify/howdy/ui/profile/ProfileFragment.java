ca8b692095b521ef10407b584837c24cb5692645
Emre Akturk <emreaktrk@hotmail.com>
2018-01-25 09:31:04 +0300

    Profile added

---
+++ app/src/main/java/com/codify/howdy/ui/profile/ProfileFragment.java
@@ -0,0 +1,13 @@
+package com.codify.howdy.ui.profile;
+
+import com.codify.howdy.HowdyFragment;
+import com.codify.howdy.R;
+
+
+public final class ProfileFragment extends HowdyFragment {
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.layout_profile;
+    }
+}
---
+++ app/src/main/java/com/codify/howdy/ui/profile/ProfileView.java
@@ -0,0 +1,6 @@
+package com.codify.howdy.ui.profile;
+
+import com.codify.howdy.ui.base.MvpView;
+
+interface ProfileView extends MvpView {
+}
