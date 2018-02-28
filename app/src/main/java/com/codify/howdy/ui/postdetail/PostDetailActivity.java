20b316f3dd6bccc973ecf92b705dc4738be5eb7c
Emre Akturk <emreaktrk@hotmail.com>
2018-02-28 17:18:55 +0300

    Post Detail added

---
+++ app/src/main/java/com/codify/howdy/ui/postdetail/PostDetailActivity.java
@@ -0,0 +1,12 @@
+package com.codify.howdy.ui.postdetail;
+
+import com.codify.howdy.HowdyActivity;
+import com.codify.howdy.R;
+
+public final class PostDetailActivity extends HowdyActivity {
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.layout_post_detail;
+    }
+}
