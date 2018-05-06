7fba0fbeb7ff24a1bb330c8ee7185a74a34bd5dd
Emre Akturk <emreaktrk@hotmail.com>
2018-05-06 04:21:21 +0300

    Bugfix

--- app/src/main/java/istanbul/codify/muudy/api/pojo/response/DeletePostResponse.java
+++ app/src/main/java/istanbul/codify/muudy/api/pojo/response/DeletePostResponse.java
@@ -1,6 +1,6 @@
 package istanbul.codify.muudy.api.pojo.response;
 
-import istanbul.codify.muudy.model.ApiResult;
+import istanbul.codify.muudy.model.Result;
 
-public final class DeletePostResponse extends BaseResponse<ApiResult> {
+public final class DeletePostResponse extends BaseResponse<Result> {
 }
